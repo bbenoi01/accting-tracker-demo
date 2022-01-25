a9425039c622747894f3b365ca8998d56e53e421
Brandon Benoit <b.nutty@gmail.com>
2022-01-25 14:43:10 -0800

    updated baseURL to heroku

--- src/js/api/accountingApi.js
+++ src/js/api/accountingApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:3005',
+	baseURL: 'https://acct-demo-backend.herokuapp.com',
 });
 
 instance.interceptors.request.use(
